207072651b85ff0b71e78eb8344bdddbdc4aa54c
PhamTuanVu31032003 <ptvu310303@gmail.com>
2024-10-12 20:26:34 +0700

    ad
    ad

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myArray ={1,2,3,4,5,6,7,8,9};
+        Console.WriteLine("Gia tri lon nhat la {0}",MaxFunc(myArray));
+    }
+    private static int MaxFunc(int [] arr){
+        int max = arr[0];
+        for(var i = 0; i < arr.Length;i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
